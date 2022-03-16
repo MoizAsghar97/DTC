71206c501e8dbfb518832651c823e54fc4fa1c90
MoizAsghar97 <moizasghar97@gmail.com>
2022-03-16 16:26:33 +0200

    assets and libraries
    assets and libraries

---
+++ Assets/Script/NewBehaviourScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+public class NewBehaviourScript : MonoBehaviour, IPointerClickHandler
+{
+    // public Image image;
+        public Image image ;
+        public string gameObjectName ;
+
+        public void Awake()
+        {
+            image = GameObject.Find(gameObjectName).GetComponent<Image>();
+          
+        }
+        
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            Debug.Log("aaaaaaaaaa");
+            image.enabled = true;
+            
+        
+        }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+         image.enabled = false ;
+       
+    
+    
+    }
+
+}
---
+++ Assets/Script/Timer.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public float timeRemaining = 10;
+    public bool  timerIsRunning = false;
+    public Text  timeText;
+
+    public float tmpTime = 500;
+    private void Start()
+    {
+        // Starts the timer automatically
+        timerIsRunning = true;
+        timeRemaining = 300;
+   
+    }
+
+    void Update()
+    {
+        if (timerIsRunning)
+        {
+     
+            if (timeRemaining > 0)
+            {
+         
+               
+                timeRemaining = timeRemaining - Time.deltaTime ;
+                System.Math.Round(timeRemaining,2);
+             
+
+                DisplayTime(timeRemaining);
+               // Debug.Log(Time.deltaTime+" Time "+ timeRemaining);
+            }
+            else
+            {
+             //   Debug.Log("Time has run out!"+timeRemaining);
+                timeRemaining = 0;
+                timerIsRunning = false;
+       
+     
+            }
+        }
+    }
+
+    void DisplayTime(float timeToDisplay)
+    {
+        timeToDisplay += 1;
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60); 
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+        // Debug.Log("Time has run out!"+string.Format("{0:00}:{1:00}", minutes, seconds));
+       
+        timeText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+}
